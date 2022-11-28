c4353c0fc11e58417bddce17acb631b445da1345
Patric Eckhart <mail@patriceckhart.com>
2022-11-28 15:25:23 +0100

    check stocklevel property if node exist

--- Classes/Service/StockService.php
+++ Classes/Service/StockService.php
@@ -5,46 +5,49 @@ use Neos\Flow\Annotations as Flow;
 use Doctrine\ORM\Mapping as ORM;
 use Neos\Eel\FlowQuery\FlowQuery;
 use Neos\Eel\FlowQuery\Operations;
+use NeosRulez\Shop\Domain\Model\Cart;
+use Neos\ContentRepository\Domain\Service\ContextFactoryInterface;
 
 /**
- * Class StockService
- *
  * @Flow\Scope("singleton")
  */
-class StockService {
+class StockService
+{
 
     /**
      * @Flow\Inject
-     * @var \NeosRulez\Shop\Domain\Model\Cart
+     * @var Cart
      */
     protected $cart;
 
     /**
      * @Flow\Inject
-     * @var \Neos\ContentRepository\Domain\Service\ContextFactoryInterface
+     * @var ContextFactoryInterface
      */
     protected $contextFactory;
-
 
     /**
      * @return void
      */
-    public function execute() {
+    public function execute(): void
+    {
         $items = $this->cart->items;
         $context = $this->contextFactory->create();
         foreach ($items as $item) {
             if(array_key_exists('node', $item)) {
                 $nodeIdentifier = $item['node'];
                 $node = $context->getNodeByIdentifier($nodeIdentifier);
-                if($node->hasProperty('stockLevel')) {
-                    $stockLevel = $node->getProperty('stockLevel');
-                    $stockManagement = (int) $node->getProperty('stockManagement');
-                    if($stockManagement) {
-                        $orderedQuantity = (int) $item['quantity'];
-                        $newQuantity = ($stockLevel - $orderedQuantity);
-                        $node->setProperty('stockLevel', $newQuantity);
-                        if($newQuantity <= 0) {
-                            $node->setProperty('stock', false);
+                if($node !== null) {
+                    if($node->hasProperty('stockLevel')) {
+                        $stockLevel = $node->getProperty('stockLevel');
+                        $stockManagement = (int) $node->getProperty('stockManagement');
+                        if($stockManagement) {
+                            $orderedQuantity = (int) $item['quantity'];
+                            $newQuantity = ($stockLevel - $orderedQuantity);
+                            $node->setProperty('stockLevel', $newQuantity);
+                            if($newQuantity <= 0) {
+                                $node->setProperty('stock', false);
+                            }
                         }
                     }
                 }
