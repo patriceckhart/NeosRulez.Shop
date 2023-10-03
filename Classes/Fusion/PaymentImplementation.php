46f6b9e699292117022427798d49f05cdcfaee1e
Patric Eckhart <mail@patriceckhart.com>
2023-10-03 10:04:17 +0200

    bugfix: error if no payment methods exist

--- Classes/Fusion/PaymentImplementation.php
+++ Classes/Fusion/PaymentImplementation.php
@@ -5,12 +5,14 @@ use Neos\Flow\Annotations as Flow;
 use Neos\Fusion\FusionObjects\AbstractFusionObject;
 use Neos\Eel\FlowQuery\FlowQuery;
 use Neos\Eel\FlowQuery\Operations;
+use Neos\ContentRepository\Domain\Service\ContextFactoryInterface;
 
-class PaymentImplementation extends AbstractFusionObject {
+class PaymentImplementation extends AbstractFusionObject
+{
 
     /**
      * @Flow\Inject
-     * @var \Neos\ContentRepository\Domain\Service\ContextFactoryInterface
+     * @var ContextFactoryInterface
      */
     protected $contextFactory;
 
@@ -23,22 +25,26 @@ class PaymentImplementation extends AbstractFusionObject {
      * @param array $settings
      * @return void
      */
-    public function injectSettings(array $settings) {
+    public function injectSettings(array $settings)
+    {
         $this->settings = $settings;
     }
 
     /**
      * @return array
      */
-    public function evaluate() {
+    public function evaluate(): array
+    {
         $context = $this->contextFactory->create();
         $available_payments = [];
         if(array_key_exists('Payment', $this->settings)) {
             $payments = $this->settings['Payment'];
             foreach ($payments as $key => $payment) {
-                $success_page = $context->getNodeByIdentifier(array_key_exists('success_page', $payment['props']) ? $payment['props']['success_page'] : false);
-                $failure_page = $context->getNodeByIdentifier(array_key_exists('failure_page', $payment['props']) ? $payment['props']['failure_page'] : false);
-                $available_payments[] = ['identifier' => $key, 'label' => $payment['props']['label'], 'icon' => $payment['props']['icon'], 'success_page' => $success_page, 'failure_page' => $failure_page];
+                if(array_key_exists('props', $payment)) {
+                    $success_page = $context->getNodeByIdentifier(array_key_exists('success_page', $payment['props']) ? $payment['props']['success_page'] : false);
+                    $failure_page = $context->getNodeByIdentifier(array_key_exists('failure_page', $payment['props']) ? $payment['props']['failure_page'] : false);
+                    $available_payments[] = ['identifier' => $key, 'label' => $payment['props']['label'], 'icon' => $payment['props']['icon'], 'success_page' => $success_page, 'failure_page' => $failure_page];
+                }
             }
         }
         return $available_payments;
